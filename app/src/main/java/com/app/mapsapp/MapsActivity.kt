89bc9fc11aa29c4c4d799517dab4cab98b420d80
aditya <adisingh925@gmail.com>
2021-12-16 18:26:28 +0530

    added google maps SDK

---
+++ app/src/main/java/com/app/mapsapp/MapsActivity.kt
@@ -0,0 +1,39 @@
+package com.app.mapsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.app.mapsapp.databinding.ActivityMapsBinding
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Kanpur and move the camera
+        val kanpur = LatLng(26.4499,80.3319)
+        mMap.addMarker(MarkerOptions().position(kanpur).title("This is where i live"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(kanpur,10f))
+    }
+}
